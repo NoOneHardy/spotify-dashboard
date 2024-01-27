067c9f6d61ee3bb79b2ec39e545f6bc4cdc03fa5
Silas Noah Hardegger <silas.hardegger@outlook.com>
2024-01-27 14:10:47 +0100

    Interactive Input Slider

---
+++ src/app/spotify/components/slider-input/slider-input.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgStyle} from "@angular/common";
+
+@Component({
+  selector: 'app-slider-input',
+  standalone: true,
+  imports: [
+    NgStyle
+  ],
+  templateUrl: './slider-input.component.html',
+  styleUrl: './slider-input.component.css'
+})
+export class SliderInputComponent {
+  @Input() progress: number | undefined = 100
+  @Input() max: number | undefined = 100
+  @Input() min = 0
+  @Output() valueChanged = new EventEmitter<number>()
+
+  valueChange(value: string) {
+    this.progress = Number(value)
+    this.valueChanged.emit(Number(value))
+  }
+}
